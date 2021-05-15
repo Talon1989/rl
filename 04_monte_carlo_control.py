3ee4c29afc46d6266f4f243d80cb0939935d82dd
Talon1989 <olivetto1989@mail.com>
2021-05-15 16:31:05 +0200

    .

--- 04_monte_carlo_control.py
+++ 04_monte_carlo_control.py
@@ -9,21 +9,16 @@ env = gym.make('Blackjack-v0')
 env.seed(0)
 
 
-#  ON-POLICY
-
-
-Q_ = defaultdict(float)  # q-values (action-values)
-total_return_ = defaultdict(float)  # total return of q(s,a)
-N_ = defaultdict(int)  # number of times (s, a) pair is visited
-
-
 def epsilon_greedy_policy(q, state, e=0.5):
     if random.uniform(0, 1) < e:
         return env.action_space.sample()
     else:
-        return max(
-            list(range(env.action_space.n)),
-            key=lambda x: q[(state, x)]
+        # return max(
+        #     list(range(env.action_space.n)),
+        #     key=lambda a: q[(state, a)]
+        # )
+        return np.argmax(
+            q[(state, a)] for a in range(env.action_space.n)
         )
 
 
@@ -40,24 +35,28 @@ def generate_episode(q):
     return episode
 
 
-n_iter = 100_000
-for i in range(n_iter):
-    episode = generate_episode(Q_)
-    state_action_pairs = [(s, a) for s, a, _ in episode]
-    rewards = [r for _, _, r in episode]
-    for t, (state, action, _) in enumerate(episode):
-        if not (state, action) in state_action_pairs[0:t]:  # using first visit
-            R = sum(rewards[t:])
-            total_return_[(state, action)] += R
-            N_[(state, action)] += 1
-            Q_[(state, action)] = total_return_[(state, action)] / N_[(state, action)]
-df = pd.DataFrame(Q_.items(), columns=['(s, a) pair', 'value'])
+#  ON-POLICY
+def control_on_policy(n_iter=100_000):
+
+    Q_ = defaultdict(float)  # q-values (action-values)
+    total_return_ = defaultdict(float)  # total return of q(s,a)
+    N_ = defaultdict(int)  # number of times (s, a) pair is visited
+
+    for i in range(n_iter):
+        episode = generate_episode(Q_)
+        state_action_pairs = [(s, a) for s, a, _ in episode]
+        rewards = [r for _, _, r in episode]
+        for t, (state, action, _) in enumerate(episode):
+            if not (state, action) in state_action_pairs[0:t]:  # using first visit
+                R = sum(rewards[t:])
+                total_return_[(state, action)] += R
+                N_[(state, action)] += 1
+                Q_[(state, action)] = total_return_[(state, action)] / N_[(state, action)]
+
+    return pd.DataFrame(Q_.items(), columns=['(s, a) pair', 'value'])
 
 
-
-
-
-
+df = control_on_policy()
 
 
 
