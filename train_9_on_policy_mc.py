00aef3edb3c39790228f589713bbd32244802d90
Talon1989 <olivetto1989@mail.com>
2021-05-22 13:10:53 +0200

    .

--- train_9_on_policy_mc.py
+++ train_9_on_policy_mc.py
@@ -65,13 +65,73 @@ def monte_carlo_every_visit(n_iter=10_000):
     )
 
 
-data = monte_carlo_every_visit()
+# data = monte_carlo_every_visit()
 
 
+env = gym.make('FrozenLake-v0')
+n_actions = env.action_space.n
+n_states = env.observation_space.n
+P = env.P
 
 
+def value_iteration(gamma=0.99):
+    threshold = 1e-20
+    state_table = np.zeros(n_states)
+    while True:
+        prev_state_table = state_table.copy()
+        for s in range(n_states):
+            q_values = [
+                np.sum([
+                    (r + gamma * prev_state_table[s_]) * p
+                    for p, s_, r, _ in P[s][a]
+                ]) for a in range(n_actions)
+            ]
+            state_table[s] = np.max(q_values)
+        if np.sum(np.abs(prev_state_table - state_table)) < threshold:
+            break
+    policy = []
+    for s in range(n_states):
+        q_values = [
+            np.sum([
+                (r + gamma * prev_state_table[s_]) * p
+                for p, s_, r, _ in P[s][a]
+            ]) for a in range(n_actions)
+        ]
+        policy.append(np.argmax(q_values))
+    return np.array(policy), state_table
 
 
+def policy_iteration(gamma=0.99):
+    threshold = 1e-20
+    policy = np.random.randint(0, n_actions, n_states)
+    while True:
+        state_table = np.zeros(n_states)
+        while True:
+            prev_state_table = state_table.copy()
+            for s in range(n_states):
+                state_table[s] = np.sum([
+                    (r + gamma * prev_state_table[s_]) * p
+                    for p, s_, r, _ in P[s][policy[s]]
+                ])
+            if np.sum(np.abs(prev_state_table - state_table)) < threshold:
+                break
+        new_policy = []
+        for s in range(n_states):
+            q_values = [
+                np.sum([
+                    (r + gamma * prev_state_table[s_]) * p
+                    for p, s_, r, _ in P[s][a]
+                ]) for a in range(n_actions)
+            ]
+            new_policy.append(np.argmax(q_values))
+        new_policy = np.array(new_policy)
+        if (policy == new_policy).all():
+            break
+        policy = new_policy
+    return policy, state_table
+
+
+policy_, state_table_ = policy_iteration()
 
 
 
