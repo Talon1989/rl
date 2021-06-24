7f8343c701fc0955054755a364f3896248ae8d62
Talon1989 <olivetto1989@mail.com>
2021-06-24 17:18:15 +0200

    .

--- 07_mab.py
+++ 07_mab.py
@@ -9,6 +9,9 @@ n_actions = env.action_space.n
 # print(env.p_dist)  # [0.8, 0.2]
 
 
+#  E-GREEDY  ############################
+
+
 count = np.zeros(n_actions)
 sum_rewards = np.zeros(n_actions)
 q = np.zeros(n_actions)
@@ -22,24 +25,70 @@ def epsilon_greedy(epsilon=0.1):
         return np.argmax(q)
 
 
+# for i in range(n_rounds):
+#     action = epsilon_greedy(0.5)
+#     _, r, _, _ = env.step(action)
+#     count[action] += 1
+#     sum_rewards[action] += r
+#     q[action] = sum_rewards[action] / count[action]
+
+
+#  SOFTMAX  ############################
+
+
+count = np.zeros(n_actions)
+sum_rewards = np.zeros(n_actions)
+q = np.zeros(n_actions)
+n_rounds = 1_000
+
+
+def softmax(T):
+    denom = np.sum([
+        np.exp(i/T) for i in q
+    ])
+    p = [np.exp(i/T) / denom for i in q]
+    arm = np.random.choice(n_actions, p=p)
+    return arm
+
+
+T = 50
+
+
+# for i in range(n_rounds):
+#     a = softmax(T)
+#     s_, r, done, _ = env.step(a)
+#     sum_rewards[a] += r
+#     count[a] += 1
+#     q[a] = sum_rewards[a] / count[a]
+#     if T > 1:
+#         T = T * 99/100
+
+
+#  CUSTOM SOFTMAX  ############################
+
+
+count = np.zeros(n_actions)
+sum_rewards = np.zeros(n_actions)
+q = np.zeros(n_actions)
+n_rounds = 1_000
+
+
+def softmax(T):
+    denom = np.sum([
+        np.exp(i*T) for i in q
+    ])
+    p = [np.exp(i*T) / denom for i in q]
+    arm = np.random.choice(n_actions, p=p)
+    return arm
+
+
 for i in range(n_rounds):
-    action = epsilon_greedy(0.5)
-    _, r, _, _ = env.step(action)
-    count[action] += 1
-    sum_rewards[action] += r
-q = sum_rewards / count
+    a = softmax(T=i/n_rounds)
+    _, r, _, _ = env.step(a)
+    sum_rewards[a] += r
+    count[a] += 1
+    q[a] = sum_rewards[a] / count[a]
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+#  UCB  ############################
 
