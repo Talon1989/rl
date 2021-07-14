0cb419358ee5a89b08e57d3f8be2ea05ae7dd64b
Talon1989 <olivetto1989@mail.com>
2021-07-14 10:23:25 +0200

    .

--- 10_tf_keras.py
+++ 10_tf_keras.py
@@ -38,12 +38,33 @@ from sklearn.model_selection import train_test_split
 #  MNIST DIGIT CLASSIFICATION USING TENSORFLOW  ###########################
 
 
-mnist = tf.keras.datasets.mnist
+mnist = keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
+#  normalizing data
+x_train, x_test = x_train / 255., x_test / 255.
+
+#  normalizing data casting it into an EagerTensor, then use tf.keras not keras
+# x_train, x_test = tf.cast(x_train / 255., tf.float32), tf.cast(x_test / 255., tf.float32)
+# y_train, y_test = tf.cast(y_train, tf.int64), tf.cast(y_test, tf.int64)
 
 
-
+#  vanilla dnn
+model = keras.models.Sequential([
+    keras.layers.Flatten(),
+    keras.layers.Dense(256, activation='relu'),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
+])
+# model = tf.keras.models.Sequential()
+# model.add(tf.keras.layers.Flatten())
+# model.add(tf.keras.layers.Dense(256, activation="relu"))
+# model.add(tf.keras.layers.Dense(128, activation="relu"))
+# model.add(tf.keras.layers.Dense(10, activation="softmax"))
+model.compile(optimizer='sgd', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(x_train, y_train, batch_size=32, epochs=10)
+# model.fit(x_train, y_train, steps_per_epoch=32, epochs=10)
+model.evaluate(x_test, y_test)
 
 
 
